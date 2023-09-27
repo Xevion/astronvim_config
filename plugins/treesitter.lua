67f34429187f2f86b0d8a5e7a8e58ea4f0b2ddc7
Xevion <xevion@xevion.dev>
2023-09-27 01:28:03 -0500

    Add languages to ensure_installed on treesitter config

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -3,7 +3,10 @@ return {
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua", "typescript", "rust", "javascript", "vim", "markdown", "html", "css", "java", "c_sharp", "cpp", "diff", "c", "bash", 
+      "astro", "cmake", "clojure", "elm", "elixir", "erlang", "gdscript", "graphql", "http", "json", "json5", "kotlin", "luadoc", 
+      "make", "nix", "ocaml", "passwd", "prisma", "python", "proto", "r", "regex", "ruby", "scala", "scss", "svelte", "sql", 
+      "terraform", "toml", "tsx", "vue", "xml", "yaml", "zig", "javascript"
     })
   end,
 }
