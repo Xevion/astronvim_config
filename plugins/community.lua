01b3582eb246e638b0cac475556870ef431b81cb
Xevion <xevion@xevion.dev>
2023-10-16 08:29:59 -0500

    Add nvim-scrollbar, neoscroll-nvim, nvim-surround & vim-wakatime plugins from Astrocommunity

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,10 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim"},
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.media.vim-wakatime" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
