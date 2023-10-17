ac465c1bf1a0ee2a9ba3e4825a10cee7192c333e
Xevion <xevion@xevion.dev>
2023-10-16 21:08:13 -0500

    Add language packs, todo-comments-nvim, trouble-nvim

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,10 +3,24 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.proto" },
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.scrolling.nvim-scrollbar" },
   { import = "astrocommunity.scrolling.neoscroll-nvim"},
   { import = "astrocommunity.motion.nvim-surround" },
-  { import = "astrocommunity.media.vim-wakatime" }
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" }
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
