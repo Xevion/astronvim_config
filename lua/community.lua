5d198e32836030ac765b95249c9edf9fe470907f
Xevion <xevion@xevion.dev>
2024-06-20 20:43:47 -0500

    enable and import previous astrocommunity plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,5 +6,49 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.proto" },
+  { import = "astrocommunity.pack.cpp" },
+  {
+    import = "astrocommunity.media.presence-nvim",
+    opts = {
+      client_id = "793271441293967371",
+      main_image = "file",
+    },
+  },
+  { import = "astrocommunity.git.git-blame-nvim" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.scrolling.nvim-scrollbar" },
+  { import = "astrocommunity.scrolling.neoscroll-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.motion.mini-move" },
+  { import = "astrocommunity.media.vim-wakatime" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
+  {
+    import = "astrocommunity.completion.copilot-lua-cmp",
+    opts = {
+      suggestion = {
+        keymap = {},
+      },
+    },
+  },
+  {
+    import = "astrocommunity.editing-support.todo-comments-nvim",
+    opts = {
+      colors = {
+        info = { "#0D0D0F" },
+      },
+    },
+  },
 }
